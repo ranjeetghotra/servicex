2cf6936aa06974bca1d296cdd1e2d2efe9942488
Ranjt Singh <ranjeetghotra6@gmail.com>
2025-12-08 22:43:56 +0530

    disable akter

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ ServiceModel.hasMany(AppointmentModel, {
   as: 'appointments',
 });
 
-sequelize.sync({ alter: true });
+sequelize.sync();
 
 // Express setup
 app.use(express.json({ limit: '50mb' }));
